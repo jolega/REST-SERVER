410949d462343eb16b163531ef87cb423ef2dc3d
jolega <alonso_juancho@hotmail.com>
2022-06-20 14:43:44 -0500

    modelo server

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
+const Server = require("./models/server");
 
-const express = require('express')
-const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
-app.listen( process.env.PORT )
+
+const server = new Server () ;
+
+server.listen();
